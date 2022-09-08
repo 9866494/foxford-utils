64d35110f0c232d4d3b103ce828df00618db55b6
awful85 <9866494@gmail.com>
2022-09-09 00:33:40 +0300

    Fix homework schedule.

--- run.py
+++ run.py
@@ -19,7 +19,7 @@ def index():
     storage_path = sript_path + '/var/ics/'
     ics_file_path = '{0}{1}.ics'.format(storage_path, schedule_hash)
 
-    if os.path.exists(ics_file_path):
+    if False and os.path.exists(ics_file_path):
         result = Path(ics_file_path).read_text()
     else:
         dict = json.loads(data.decode("utf-8"))
@@ -55,26 +55,30 @@ def index():
 
             vevents.append(vevent)
 
-            if prior_date != None and prior_date.date() < starts_at.date():
-                home_work_starts_at = starts_at + \
-                    timedelta(minutes=home_work_timedelta)
-                home_work_ends_at = home_work_starts_at + \
-                    timedelta(minutes=home_work_duration)
+            if prior_date != None:
+                truncated_prior_date = prior_date.replace(hour=0, minute=0, second=0, microsecond=0)
+                truncated_starts_at = starts_at.replace(hour=0, minute=0, second=0, microsecond=0)
+                
+                if truncated_prior_date < truncated_starts_at:
+                    home_work_starts_at = prior_date + \
+                        timedelta(minutes=home_work_timedelta)
+                    home_work_ends_at = home_work_starts_at + \
+                        timedelta(minutes=home_work_duration)
 
-                home_work_date = home_work_starts_at.strftime("%Y%m%d")
-                home_work_dtstart = home_work_starts_at.strftime(
-                    "%Y%m%dT%H%M%SZ")
-                home_work_dtend = home_work_ends_at.strftime("%Y%m%dT%H%M%SZ")
-                home_work_dtstamp = home_work_dtstart
+                    home_work_date = home_work_starts_at.strftime("%Y%m%d")
+                    home_work_dtstart = home_work_starts_at.strftime(
+                        "%Y%m%dT%H%M%SZ")
+                    home_work_dtend = home_work_ends_at.strftime("%Y%m%dT%H%M%SZ")
+                    home_work_dtstamp = home_work_dtstart
 
-                home_work_uid = '{0}@{1}'.format(user_id, home_work_date)
+                    home_work_uid = '{0}@{1}'.format(user_id, home_work_date)
 
-                vevent = template('vevent', dtstart=home_work_dtstart, dtend=home_work_dtend,
-                                  dtstamp=home_work_dtstamp, uid=home_work_uid, description=home_work_description, summary=home_work_summary, location=home_work_location)
+                    vevent = template('vevent', dtstart=home_work_dtstart, dtend=home_work_dtend,
+                                    dtstamp=home_work_dtstamp, uid=home_work_uid, description=home_work_description, summary=home_work_summary, location=home_work_location)
 
-                vevents.append(vevent)
+                    vevents.append(vevent)
 
-            prior_date = starts_at
+            prior_date = ends_at
 
         result = template('vevents', vevents=vevents)
 
